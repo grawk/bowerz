70856ef275ba26f5c46792c028a85fac28aedee5
Matt Edelman <medelman@paypal.com>
2014-05-05 16:53:19 -0700

    adding symlink task

---
+++ tasks/symlink.js
@@ -0,0 +1,33 @@
+'use strict';
+var path = require('path');
+
+module.exports = function bower(grunt) {
+	// Load task
+	grunt.loadNpmTasks('grunt-contrib-symlink');
+
+	// Options
+	return {
+		options: {
+			overwrite: true
+		},
+		expanded: {
+			files: [{
+				expand: true,
+				overwrite: true,
+				flatten: false,
+				cwd: 'pusblic/components',
+				src: ['*/*/*.dust'],
+				regex: /(public\/components)(\/.+)(\/templates)(\/.+dust)/g,
+				backreference: 'public/templates/components$2$4'
+			}, {
+				expand: true,
+				overwrite: true,
+				flatten: false,
+				cwd: 'public/components/',
+				src: ['*/*/*/*/*.properties'],
+				regex: /(public\/components)(\/.+)(\/locales)(\/[A-Z]{2}\/[a-z]{2})(\/.+properties)/g,
+				backreference: 'locales$4/components$2$5'
+			}]
+		}
+	};
+};
